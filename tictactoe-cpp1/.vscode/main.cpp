d53dc8abcf5c7efb83fcdca04d92ada3c5a5dd2b
userone <brianc2788@gmail.com>
2021-08-14 09:51:14 -0400

    building projects with vs-code

---
+++ tictactoe-cpp1/.vscode/main.cpp
@@ -0,0 +1,9 @@
+/*
+and again
+*/
+#include <iostream>
+#include "ttt.hh"
+
+int main(int argc, char* argv[]){
+    return 0;
+}
---
+++ tictactoe-cpp1/.vscode/ttt.hh
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef TTT_H
+#define TTT_H
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+class ttt {
+public:
+    ttt();
+    ~ttt();
+
+    void print_test();
+private:
+};
+
+#endif /* TTT_H */
