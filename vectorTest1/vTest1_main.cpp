7f1eee68c0ff36302f23b44e2689d26696068ded
userone <brianc2788@gmail.com>
2021-12-01 03:10:18 -0500

    Compiled binary.

--- vectorTest1/vTest1_main.cpp
+++ vectorTest1/vTest1_main.cpp
@@ -1,19 +1,24 @@
-// vector refresh
+// vector refresher
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main(int argc,char* argv[]){
-    std::cout << "this is a test." << std::endl;
+    std::cout << "this is a test. with vectors. i mean, one. just one vector." << std::endl;
     std::vector<std::string> sVector;
-    std::cout << "Enter a string: ";
+    std::cout << "Enter a string.\n"
+              << "To end input:\n"
+              << "Windows: Ctrl+Z\n"
+              << "Other: Ctrl+D\n";
 
     for (std::string s;std::cin>>s;){
         sVector.push_back(s);
     }
 
-    std::cout << "got your string." << std::endl
+    std::cout << "\ngot your string." << std::endl
               << "you typed:" << std::endl;
+
+    // iterate through the str vector.
     for (std::string el : sVector){
         std::cout << el << std::endl;
     }
