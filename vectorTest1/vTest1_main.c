2fd8524bc435c3eeceaca07257fbc20386872938
userone <brianc2788@gmail.com>
2021-12-05 22:57:25 -0500

    added/removed files in vectorTest1.

--- vectorTest1/vTest1_main.c
+++
@@ -1,6 +0,0 @@
-// ... the C version. JK BCUZ THERE'S NO VECTORS! DUH!
-#include <stdio.h>
-int main(int argc,char* argv[]){
-	printf("um, jk because there are no vectors in C - Duh.\n");
-	return 0;
-}
---
+++ vectorTest1/vTest2.cpp
@@ -0,0 +1,28 @@
+// Just wanted to use the std namespace
+// for readablity. Not sure why I'm so
+// averse to using it.
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    cout << "Vector Test 2" << endl;
+
+    string s1 = "";
+    for(string word;cin>>word;){
+        s1 += word;
+        s1 += " ";
+        cout << "debug: " << s1 << endl;
+    }
+
+    string s2 = "";
+
+    for (int n = 0; n < (s1.length()-1);++n){
+        s2 += s1[n];
+    }
+
+    cout << endl << s2 << "." << endl;
+
+    return 0;
+}
