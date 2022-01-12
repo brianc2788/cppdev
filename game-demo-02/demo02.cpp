377217a22630831d441c38cb70fe4e6efa3648e6
userone <brianc2788@gmail.com>
2022-01-12 15:14:52 -0500

    Edits to game-demo-02 project.

--- game-demo-02/demo02.cpp
+++ game-demo-02/demo02.cpp
@@ -14,29 +14,30 @@ const int screenW = 640;
 const int screenH = 480;
 
 SDL_Window* pWin1 = nullptr;
-SDL_Renderer* pRenderer1 = nullptr;
+SDL_Renderer* pRendererMain = nullptr;
 
-bool init(); //init SDL subsystems, create window & renderer.
-void quit(); //free resources & quit.
+bool init();    //init SDL subsystems, create window & renderer.
+void close();    //free resources & quit.
+void render(); //clear & present renderer.
+bool events();  //handle events. temporarily, returns a boolean.
 
 int main(int argc,char* argv[]){
     if(init()){
-        // Set draw color to black and present the window.
-        SDL_SetRenderDrawColor(pRenderer1, 0,0,0,255);
-        SDL_RenderClear(pRenderer1);
-        SDL_RenderPresent(pRenderer1);
-
-        // sleep 10 seconds.
-        SDL_Delay(10000);
+        bool bRunning = true;
+        while(bRunning){
+            bRunning = events();
+            render();
+            SDL_Delay(500); //give the cpu a break...
+        }
     }
     else{
         printf("Aborting...\n");
-        quit();
+        close();
         return 1;
     }
 
     // free resources & quit.
-    quit();
+    close();
     printf("DEBUG: Exiting without error...\n");
 
     return 0;
@@ -51,8 +52,8 @@ bool init(){
                                  screenW,screenH,
                                  SDL_WINDOW_SHOWN);
         if(pWin1 != nullptr){
-            pRenderer1 = SDL_CreateRenderer(pWin1,-1,0); //SDL_RENDERER_ACCELERATED flag use?
-            if(pRenderer1 != nullptr){
+            pRendererMain = SDL_CreateRenderer(pWin1,-1,SDL_RENDERER_ACCELERATED);
+            if(pRendererMain != nullptr){
                 return true;
             }
         }
@@ -64,8 +65,27 @@ bool init(){
 }
 
 // Free the window & renderer. Quit SDL2.
-void quit(){
+void close(){
     SDL_DestroyWindow(pWin1);
-    SDL_DestroyRenderer(pRenderer1);
+    SDL_DestroyRenderer(pRendererMain);
     SDL_Quit();
+}
+
+void render(){
+    SDL_SetRenderDrawColor(pRendererMain, 0x0,0x0,0x0,0xFF);
+    SDL_RenderClear(pRendererMain);
+    SDL_RenderPresent(pRendererMain);
+}
+
+bool events(){
+    SDL_Event event;
+    while(SDL_PollEvent(&event) != 0){
+        if(event.type == SDL_QUIT){
+            return false;
+        }
+        else{
+            continue;
+        }
+    }
+    return true;
 }
