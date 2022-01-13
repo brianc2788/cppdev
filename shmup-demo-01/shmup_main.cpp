177fd16d29d4859caa806693b40b61ea3155ad89
userone <brianc2788@gmail.com>
2022-01-13 18:52:30 -0500

    Added project: shmup-demo-01; skeletal outline.

---
+++ shmup-demo-01/shmup_main.cpp
@@ -0,0 +1,11 @@
+/*************************************************
+# SHMUP DEMO 01
+# brianc2788@gmail.com
+*************************************************/
+#include <stdio.h>
+#include "engine1.h"
+
+int main(int argc,char* argv[]){
+    Engine1 e1;
+    return 0;
+}
