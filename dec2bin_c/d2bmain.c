8a5713fc53155c168cb500202b41a383a2407036
userone <brianc2788@gmail.com>
2022-01-30 21:21:12 -0500

    Completed and added fixes to dec2bin C project.

--- dec2bin_c/d2bmain.c
+++ dec2bin_c/d2bmain.c
@@ -1,13 +1,15 @@
-/*************************************************
+/******************************************************
 # dec2bin - Decimal-to-Binary converter #
 Prompts user for a decimal number. Prints
 the binary equivalent.
 
 *Known bugs:
-    a. Stop printing preceeding zeroes.
+    a. Stop printing preceeding zeroes. (FIXED)
+    c. May want to try copying and/or truncating
+       the bit array.
 
 Authored by brianc2788@gmail.com
-*************************************************/
+******************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -34,17 +36,30 @@ int main(int argc,char* argv[]){
 }
 
 void PrintBitArray(int nDecimal){
-    //Create an array of bits.
-    int nIndex = 0;
-    while(nDecimal != 0){
-        nBitArray[nIndex] = (nDecimal % 2);
-        nIndex++;
-        nDecimal = nDecimal/2;
+    //If the user enters "1"...
+    if (nDecimal == 1) {
+        printf("1\n*1 bit\n\n");
     }
+    else{
+        //Create an array of bits.
+        int nIndex = 0;
+        while(nDecimal != 0){
+            nBitArray[nIndex] = (nDecimal % 2);
+            nDecimal = nDecimal/2;
+            if(nDecimal == 0){
+                continue;
+            }
+            else{
+                nIndex++;
+            }
+        }
 
-    //Print the array of bits in reverse.
-    for(int n = nIndex; n >= 0; n--){
-        printf("%d",nBitArray[n]);
+        //Print the array of bits in reverse.
+        for(int n = nIndex; n >= 0; n--){
+            printf("%d",nBitArray[n]);
+        }
+
+        //Report # of significant bits
+        printf("\n*%d bits\n\n", (nIndex+1));
     }
-    printf("\n");
 }
