8dfad3de16a6e52e3214abd95e526673319bf4a4
brianc2788 <brianc2788@gmail.com>
2023-09-23 10:46:57 -0400

    Restructured code for readability.

--- facfind/facfind.cpp
+++ facfind/facfind.cpp
@@ -1,44 +1,51 @@
-/* FACTOR FINDER VERSION 0.1 */
-/* Started on Feb 9, 2021 */
-// -application asks user for a number.
-// -application checks if the number is prime, true or false.
-// -application then enters a loop, printing factors as they are discovered.
-// -running loop ends when user wants to quit.
-//
-// -end of development
-// final summary: app prints title line, prompts user to enter a number to find its factors
-//				  or to enter a zero to quit the program. not my desired way to design the
-//				  running loop, but it's all that could be gotten to function properly.
-//				  keeping this a functioning application and will build on in a future
-//				  version.
+/*******************************************************************************
+* FACTOR FINDER
+* Get an integer from the stdin and print its factors, indicate primality.
+* https://brianc2788.github.io/
+*******************************************************************************/
 #include <iostream>
-//#include <stdio.h>
 using namespace std;
 
+void printFactors(int nTarget);
+
 int main(int argc, char* argv[]) {
-	cout << "\t*|* FACTOR FINDER - VER 0.1 *|*" << endl
-		<< endl;
+	cout << "FACFINDER" << endl;
 	bool bRunning = true;
 	while (bRunning) {
 		int nInput = 0;
-		cout << "Enter a number to find its factors or enter a zero to quit: ";
+
+		cout << "Enter a number to find its factors or enter a zero (0) to quit: ";
 		cin >> nInput; cout << endl;
 
 		if (nInput == 0) {
 			bRunning = false;
 		}
 		else {
-			bRunning = true;
-		}
-
-		cout << "Factors of " << nInput << ":" << endl;
-
-		for (int x = 1; x <= nInput; x++) {
-			if (nInput % x == 0) {
-				cout << x << endl;
-			}
+			printFactors(nInput);
 		}
 	}
 
 	return 0;
+}
+
+/* Print factors for a number and its primality. */
+void printFactors(int nTarget) {
+
+
+	int nCount = 0; // Number of factors
+
+	for (int n = 1; n <= nTarget; n++) {
+		if (nTarget % n == 0) {
+			cout << n << endl;
+			nCount++;
+		}
+	}
+
+	cout << "\nNumber of factors: " << nCount << endl;
+
+	if (nCount == 2)
+		cout << "IS PRIME" << endl;
+	else
+		cout << "NOT PRIME" << endl
+		<< endl;
 }
